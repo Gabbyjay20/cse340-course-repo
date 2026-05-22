bfdd71a1d8909d2fdd775621c805d484519884db
Gabriel James <gabbyjay20@gmai.com>
2026-05-22 21:04:28 +0100

    Fix db.js typo

--- src/models/db.js
+++ src/models/db.js
@@ -1,10 +1,14 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 import pg from "pg";
 
 const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DB_URL,
-  ssl: true
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
